a417d500c292e7dfbabbc3566b85b1f2d60069cf
Sergio <sergioandrescs@hotmail.com>
2014-05-07 16:58:04 -0500

    Aun en proceso.

--- Schroedinger/Schroedinger.c
+++ Schroedinger/Schroedinger.c
@@ -18,7 +18,8 @@ int main (){
 
   onda = fopen("onda.dat","w");
   proba = fopen ("proba.dat", "w");
-  n_points = 1000;
+  n_points = 500;
+  dx = 0.02;
 
   /*Se realiza la reserva de memoria*/
   x = malloc(n_points*sizeof(float));
@@ -30,29 +31,31 @@ int main (){
   rho = malloc (n_points*sizeof(float));
   /*Se crean los puntos en x*/
   
-  for (i=0;i<n_points;i++){
-    x[i]=(float)2.0*i/(n_points) + 4.0;
+  x[0] = -5.0;
+
+  for (i=1;i<n_points;i++){
+    x[i]=x[i-1] + dx;
   }
   /*Se crea el potencial*/
  
   for (i=0;i<n_points;i++){
-    potencial[i] = 0.5 * pow(x[i],2);
+    potencial[i] = 5.0 * pow(x[i],2);
   }
 
   /*Se crean los diferenciales que determinan la solucion*/
 
   dx = x[1]-x[0];
-  dt = 0.25*pow(dx,2);
+  dt = 0.1*pow(dx,2);
   alpha = dt/(2*pow(dx,2));
-  sigma = 0.05;
-  ko = 16*PI;
+  sigma = 0.5;
+  ko = 17*PI;
   tiempo = 2000;
 
   /*Se crea el estado inicial*/
 
   for (i=0;i<n_points;i++){
-    rcurrent[i] = exp(-0.5*pow(((x[i]-5)/sigma),2))*cos(ko*x[i]);
-    icurrent[i] = exp(-0.5*pow(((x[i]-5)/sigma),2))*sin(ko*x[i]);
+    rcurrent[i] = exp(-0.5*pow(((x[i])/sigma),2))*cos(ko*x[i]);
+    icurrent[i] = exp(-0.5*pow(((x[i])/sigma),2))*sin(ko*x[i]);
   }
 
 /*Se imprimen los puntos en x.*/
@@ -82,12 +85,12 @@ for(i=0;i<n_points;i++){
  }
  /*Se crea un loop que realice la solucion.*/
  for (j=0;j<tiempo;j++){
-   for (i=0;i<n_points;i++){
-     rnew[i] = rcurrent[i] - 2*(alpha*(icurrent[i+1]+icurrent[i-1])-2*(alpha+potencial[i]*dt)*icurrent[i]);
+   for (i=1;i<n_points-1;i++){
+     rnew[i] = rcurrent[i] - dt*(icurrent[i+1]+icurrent[i-1]-2*icurrent[i])/(dx*dx)+dt*potencial[i]*icurrent[i];
      rho[i] = (rcurrent[i]*rnew[i]) + (pow(icurrent[i],2));
    }
-   for (i=0;i<n_points;i++){
-     inew[i] =  icurrent[i] + 2*(alpha*(rcurrent[i+1]+rcurrent[i-1])-2*(alpha+potencial[i]*dt)*rcurrent[i]);
+   for (i=1;i<n_points-1;i++){
+     inew[i] =  icurrent[i] + dt*(rcurrent[i+1]+rcurrent[i-1] - 2*rcurrent[i])/(dx*dx)+dt*potencial[i]*rcurrent[i];
    }
    /*Se imprimen los datos.*/
    for(i=0;i<n_points;i++){
@@ -104,14 +107,14 @@ for(i=0;i<n_points;i++){
    }
 
    for(i=0;i<n_points;i++){
-     fprintf(proba,"%f\t", rho[i]);
+     fprintf(proba,"%f\t", rho[i]+ 0.0015*potencial[i]);
      if(i == n_points-1){
        fprintf(proba,"\n");
      }
    }
    /*Se actualiza la onda.*/
 
-   for(i=0;i<n_points;i++){
+   for(i=1;i<n_points-1;i++){
      rcurrent[i] = rnew[i];
      icurrent[i] = inew[i];
    }
